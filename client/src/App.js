6e06e422742d1fa909aced5bf9231897e4ec9a1b
eklavya-joshi <eklavya@LAPTOP-42IQ6MQ6>
2023-07-01 16:33:19 +1000

    removed unnecessary imports

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 import React from 'react';
-import {AppBar, Toolbar, TextField, 
-  IconButton, Typography, Paper, 
-  List, ListItem, Drawer,
-  ListItemText, ListItemIcon, Divider,
-  Accordion, AccordionSummary, AccordionDetails} from "@mui/material";
+import {AppBar, Toolbar, TextField, Typography, Divider} from "@mui/material";
 import {
-  experimental_sx as sx,
   ThemeProvider,
-  ThemeOptions,
-  createTheme,
 } from "@mui/material/styles";
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-import MenuIcon from "@mui/icons-material/Menu";
-import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
 import sideDrawer from "./sideDrawer.js"
 
--- client/src/sideDrawer.js
+++ client/src/sideDrawer.js
@@ -1,19 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
 import React from 'react';
-import {AppBar, Toolbar, TextField, 
-  IconButton, Typography, Paper, 
-  List, ListItem, Drawer,
-  ListItemText, ListItemIcon, Divider,
+import {Toolbar, Typography, Paper, 
+  List, ListItem, Drawer, Divider,
   Accordion, AccordionSummary, AccordionDetails} from "@mui/material";
-import {
-  experimental_sx as sx,
-  ThemeProvider,
-  ThemeOptions,
-  createTheme,
-} from "@mui/material/styles";
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-import MenuIcon from "@mui/icons-material/Menu";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
 export default function sideDrawer() {
