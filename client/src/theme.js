fc2d3279be0d7d62a29dd61e5eee7d63bac30b34
eklavya-joshi <eklavya.joshi@csesoc.org.au>
2023-07-01 22:56:49 +1000

    changed themes.js

--- client/src/theme.js
+++ client/src/theme.js
@@ -1,56 +1,56 @@
 import {
-    ThemeOptions, createTheme,
-  } from "@mui/material/styles";
+  createTheme,
+} from "@mui/material/styles";
 
 export const appBarHeight = 75;
-export const drawerWIdth = 320;
+export const drawerWIdth = "30%";
 
 export const basicTheme = createTheme({
-    palette: {
-      mode: 'light',
-      primary: {
-        main: '#45dc92',
-      },
-      secondary: {
-        main: '#f50057',
+  palette: {
+    mode: 'light',
+    primary: {
+      main: '#45dc92',
+    },
+    secondary: {
+      main: '#f50057',
+    },
+  },
+  components: {
+    MuiAccordion: {
+      styleOverrides: {
+        root: {
+          boxShadow: "none",
+        },
       },
     },
-    components: {
-      MuiAccordion: {
-        styleOverrides: {
-          root: {
-            boxShadow: "none",
-          },
+    MuiAccordionSummary: {
+      styleOverrides: {
+        root: {
+          width: `${drawerWIdth}px`,
         },
       },
-      MuiAccordionSummary: {
-        styleOverrides: {
-          root: {
-            width: `${drawerWIdth}px`,
-          },
-        },
-      },
-      MuiListItem: {
-        styleOverrides: {
-          root: {
-            padding: "0px"
-          }
+    },
+    MuiListItem: {
+      styleOverrides: {
+        root: {
+          padding: "0px"
         }
-      },
-      MuiList: {
-        styleOverrides: {
-          root: {
-            paddingTop: "0px",
-            paddingBottom: "0px",
-          }
+      }
+    },
+    MuiList: {
+      styleOverrides: {
+        root: {
+          paddingTop: "0px",
+          paddingBottom: "0px",
         }
-      },
-      MuiSvgIcon: {
-        styleOverrides: {
-          root: {
-            // color: "#45dc92",
-          },
+      }
+    },
+    MuiSvgIcon: {
+      styleOverrides: {
+        root: {
+          // color: "#45dc92",
         },
       },
-    }
-  });
+    },
+  }
+});
