664309e3130457bb6fe5ace249e3b1bb84ad07a6
eklavya-joshi <eklavya@LAPTOP-42IQ6MQ6>
2023-07-01 17:02:08 +1000

    created more files

---
+++ client/src/listItem.js
@@ -0,0 +1,30 @@
+import React from "react";
+import {Typography, ListItem, Divider,
+    Accordion, AccordionSummary, AccordionDetails} from "@mui/material";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+
+export default function listItem() {
+    return (
+        <div>
+          <ListItem>
+          {/* <ListItemIcon>
+            <AccountCircleIcon/>
+          </ListItemIcon> */}
+          <Accordion square={true}>
+          <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          >
+            <Typography>asdf</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+              malesuada lacus ex, sit amet blandit leo lobortis eget.
+            </Typography>
+          </AccordionDetails>
+          </Accordion>
+        </ListItem>
+        <Divider/>
+        </div>
+      );
+}
--- client/src/sideDrawer.js
+++ client/src/sideDrawer.js
@@ -1,33 +1,10 @@
 import React from 'react';
-import {Toolbar, Typography, Paper, 
-  List, ListItem, Drawer, Divider,
-  Accordion, AccordionSummary, AccordionDetails} from "@mui/material";
-import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import {Toolbar, Paper, 
+  List, Drawer, Divider} from "@mui/material";
+
+import listItem from "./listItem.js";
 
 export default function sideDrawer() {
-    const listItem = (
-        <div>
-          <ListItem>
-          {/* <ListItemIcon>
-            <AccountCircleIcon/>
-          </ListItemIcon> */}
-          <Accordion square={true}>
-          <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          >
-            <Typography>asdf</Typography>
-          </AccordionSummary>
-          <AccordionDetails>
-            <Typography>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-              malesuada lacus ex, sit amet blandit leo lobortis eget.
-            </Typography>
-          </AccordionDetails>
-          </Accordion>
-        </ListItem>
-        <Divider/>
-        </div>
-      );
     
     return (
       <div>
@@ -44,8 +21,8 @@ export default function sideDrawer() {
           <Paper variant="outlined">
           <React.Fragment>
             <List>
-              {listItem}
-              {listItem}
+              {listItem()}
+              {listItem()}
             </List>
           </React.Fragment>
           </Paper>
