c977dfd832d0a1a7f76dfe0c55540df5628dbef2
Shubh <Shu8h1234@gmail.com>
2023-07-01 16:35:18 +1000

    Created basic endpoints for new group and new user

--- server/server.js
+++ server/server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const sessionStorage = require('sessionstorage');
+import { v4 as uuidv4 } from 'uuid';
+import User from './models/User.js';
+import Group from './models/Group.js'
+
 
 const dotenv = require('dotenv');
 dotenv.config();
@@ -41,7 +42,47 @@ app.get('/', (req, res) => {
     res.status(200).json({ msg: "Inside GET request" });
 })
 
-
 app.use(express.json());
+
+
+app.post('/createUser', async (req, res) => {
+    try {
+      const user = new User({
+        username: req.body.username,
+        location: req.body.location
+      });
+  
+      await user.save();
+
+      res.status(200).json({ msg: 'User created successfully', user: user});
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Failed to create user' });
+    }
+  });
+  
+
+app.post('/createGroup', async (req, res) => {
+    // Generate a new Id
+    const newId = uuidv4().slice(0, 10);
+
+    const group = new User({
+        sessionId: newId,
+        groupAdmin: '',
+        users: [],
+        eventTypes: []
+    });
+
+    // Save the group to the database
+    try {
+        await group.save();
+        res.status(200).json({ msg: 'Group created successfully', id: newId, group: group});
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Failed to create group' });
+    }
+});
+
+
 
 
